cd9512c524ce4a9e9e7dd02185188e5e9ea33c76
KSJ <gmrtkf101@gmail.com>
2025-11-28 10:49:02 +0900

    feat. 콘솔 초기화 기능 구현

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //색깔 조정
+            Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            //제목 수정
+            Console.Title = "콘솔로 만든 소코반";
+            //커서 숨김
+            Console.CursorVisible = false;
+            Console.Clear();
         }
     }
 }
